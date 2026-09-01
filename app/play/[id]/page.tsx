325138165f5a24a2ce74c33c3fb799a12f4e3244
pjrus <paarangatj@gmail.com>
2026-09-01 13:01:37 +1000

    ci: deploy Puzzie to GitHub Pages

--- app/play/[id]/page.tsx
+++ app/play/[id]/page.tsx
@@ -1,6 +1,11 @@
 import { PuzzlePlayer } from "@/components/PuzzlePlayer";
 import { InvalidPuzzle } from "@/components/player/PlayerStates";
+import { puzzles } from "@/lib/puzzles/catalogue";
 import { getPuzzleById } from "@/lib/puzzles/queries";
+
+export function generateStaticParams() {
+  return puzzles.map((puzzle) => ({ id: puzzle.id }));
+}
 
 export default async function PlayPage({
   params,
